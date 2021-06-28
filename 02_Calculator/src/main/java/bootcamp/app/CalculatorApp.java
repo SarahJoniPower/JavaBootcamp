46a854c4cea3ad40ba54d6f1bceb6422c801c2ff
Sarah Power <sarahjonipower0@gmail.com>
2021-06-28 13:48:10 +0100

    Tests now passing

--- 02_Calculator/src/main/java/bootcamp/app/CalculatorApp.java
+++ 02_Calculator/src/main/java/bootcamp/app/CalculatorApp.java
@@ -1,11 +1,26 @@
 package bootcamp.app;
 
+import java.math.BigDecimal;
+import bootcamp.data.Params;
+import bootcamp.calculator.Calculator;
+
 /**
  * TODO: The application should be able to handle 3 arguments (first number, second number, operation).
  * The application should write the results to the standard output.
  */
 public class CalculatorApp {
     public static void main(final String[] args) {
-        throw new RuntimeException("Not yet implemented");
+
+        final BigDecimal x = new BigDecimal(args[0]);
+        final BigDecimal y = new BigDecimal(args[1]);
+        final String operator = args[2];
+
+        Params params = new Params(x,y,operator);
+
+        Calculator calculator = new Calculator();
+
+        BigDecimal result = calculator.calculate(params);
+
+        System.out.println(result);
     }
 }
--- 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
+++ 02_Calculator/src/main/java/bootcamp/calculator/Calculator.java
@@ -1,11 +1,37 @@
 package bootcamp.calculator;
 
 import bootcamp.data.Params;
-
 import java.math.BigDecimal;
+import java.math.MathContext;
 
 public class Calculator {
+
     public BigDecimal calculate(final Params params) {
-        return null;
+        BigDecimal result = null;
+        MathContext m = new MathContext(2);
+
+        switch (params.getOperator()) {
+            case "+": {
+                result = params.getX().add((params.getY()));
+            }
+            break;
+            case "-": {
+                result = params.getX().subtract((params.getY()));
+            }
+            break;
+            case "/": {
+                result = params.getX().divide((params.getY()));
+            }
+            break;
+            case "x": {
+                result = params.getX().multiply((params.getY()));
+            }
+            break;
+            default: {
+                throw new IllegalArgumentException("Not yet implemented");
+            }
+        }
+        return result.round(m).stripTrailingZeros();
     }
+
 }
